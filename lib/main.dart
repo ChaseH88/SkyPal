dbe968bb313a33fdd68f29aab3eb827b39b5f8f5
Chase Harrison <chaseh88@gmail.com>
2023-02-15 22:14:32 -0600

    working on widget layout

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,34 @@ class WeatherWidget extends StatefulWidget {
 class _WeatherWidgetState extends State<WeatherWidget> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final container1Height = screenHeight * 0.25; // 25% of screen height
+    final container2Height = screenHeight * 0.35; // 35% of screen height
+    final container3Height = screenHeight * 0.285; // 40% of screen height
+
     return Scaffold(
       appBar: AppBarWidget(title: 'This is a test'),
       backgroundColor: Colors.lightBlue,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+              color: Colors.transparent,
+              height: container1Height,
+              child: Text('Widget 1')),
+          Container(
+              color: Colors.transparent,
+              height: container2Height,
+              child: Text('Widget 2')),
+          Container(
+              color: Colors.transparent,
+              constraints: BoxConstraints(
+                minHeight: container3Height,
+                maxHeight: screenHeight - container1Height - container2Height,
+              ),
+              child: Text('Widget 3')),
+        ],
+      ),
     );
   }
 }
